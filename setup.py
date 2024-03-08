39852347e65413636a9dc3b25a896ca570692cad
Ariel Tubul <avarti765@gmail.com>
2024-03-08 14:01:15 +0200

    code duplication..

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 import setuptools
-from typing import List
 from pathlib import Path
-
 
 CURRENT_FOLDER = Path(__file__).parent
 README_PATH = CURRENT_FOLDER / 'README.md'
-
-
-def get_requirements() -> List[str]:
-    REQUIREMENTS_FILE_NAME = 'requirements.txt'
-    requirements_path = CURRENT_FOLDER / REQUIREMENTS_FILE_NAME
-
-    requirements_content = requirements_path.read_text()
-    return [requirement for requirement in requirements_content.splitlines() if requirement]
 
 
 setuptools.setup(
     name = "comp_camp",
     version = "1.0.0",
     author = "Ariel Tubul",
-    install_requires = get_requirements(),
+    install_requires = ['ply'],
     packages = setuptools.find_packages(),
     long_description=README_PATH.read_text(),
     long_description_content_type='text/markdown',
