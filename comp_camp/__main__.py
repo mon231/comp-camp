7025b32fafe99edd82923b316543cd07e5e8a92e
Ariel Tubul <avarti765@gmail.com>
2024-03-09 21:10:54 +0200

    print student-name and parsing-tree in __main__ file

--- comp_camp/__main__.py
+++ comp_camp/__main__.py
@@ -1,19 +1,22 @@
+from sys import stderr
 from pathlib import Path
 from argparse import ArgumentParser
-from .tokenizer import create_token_stream
+from .parser import parser
 
 CPL_FILE_SUFFIX = '.ou'
 QUD_FILE_SUFFIX = '.qud'
-
+AUTHOR_STUDENT_NAME = 'Ariel Tubul'
 
 def parse_arguments():
-    arguments_parser = ArgumentParser('Ariel Tubul\'s implementation for quad compiler')
+    arguments_parser = ArgumentParser(f'{AUTHOR_STUDENT_NAME}\'s implementation for quad compiler')
     arguments_parser.add_argument('code_file', type=Path, help=f'Path to code-file (got to end with {CPL_FILE_SUFFIX})')
     arguments_parser.add_argument('--output', '-o', type=Path, required=False, help=f'Path to output file (recommended end with {QUD_FILE_SUFFIX})')
     return arguments_parser.parse_args()
 
 
 def main():
+    print(f'Running {AUTHOR_STUDENT_NAME}\'s compiler', file=stderr)
+
     arguments = parse_arguments()
     output_file: Path = arguments.output or arguments.code_file.with_suffix(QUD_FILE_SUFFIX)
 
@@ -21,9 +24,7 @@ def main():
         raise RuntimeError(f'Unexpected suffix for input-file (should be "{CPL_FILE_SUFFIX}")')
 
     try:
-        token_stream = create_token_stream(arguments.code_file.read_text())
-        for tok in token_stream:
-            print(tok)
+        print(parser.parse(arguments.code_file.read_text()))
     except:
         output_file.unlink(missing_ok=True)
         raise
