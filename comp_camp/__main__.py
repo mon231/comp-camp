3f7793976ab81aa255566874bfa00ce4aef21180
Ariel Tubul <avarti765@gmail.com>
2024-03-10 02:14:04 +0200

    implemented skel for main file

--- comp_camp/__main__.py
+++ comp_camp/__main__.py
@@ -1,11 +1,23 @@
 from sys import stderr
 from pathlib import Path
 from argparse import ArgumentParser
+
 from .parser import parser
+from .cpl_types import Program
+from .quad_translator import QuadTranslator
 
 CPL_FILE_SUFFIX = '.ou'
 QUD_FILE_SUFFIX = '.qud'
 AUTHOR_STUDENT_NAME = 'Ariel Tubul'
+
+
+def cpl_to_quad(cpl_code: str) -> str:
+    quad_translator = QuadTranslator()
+    cpl_program: Program = parser.parse(cpl_code)
+
+    quad_code = cpl_program.translate(quad_translator)
+    return quad_code.opcodes
+
 
 def parse_arguments():
     arguments_parser = ArgumentParser(f'{AUTHOR_STUDENT_NAME}\'s implementation for quad compiler')
@@ -24,7 +36,10 @@ def main():
         raise RuntimeError(f'Unexpected suffix for input-file (should be "{CPL_FILE_SUFFIX}")')
 
     try:
-        print(parser.parse(arguments.code_file.read_text()))
+        cpl_code = arguments.code_file.read_text()
+
+        quad_code = cpl_to_quad(cpl_code)
+        output_file.write_text(quad_code)
     except:
         output_file.unlink(missing_ok=True)
         raise
