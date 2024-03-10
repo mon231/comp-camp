f428eaa6dcdb2e9c282eff0cb4ab4c06fa8fc078
Ariel Tubul <avarti765@gmail.com>
2024-03-10 08:29:09 +0200

    implemented switch-case statement

--- comp_camp/cpl_types.py
+++ comp_camp/cpl_types.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Dict
+from dataclasses import dataclass
 from .quad_translator import QuadTranslator, QuadTranslatable, QuadCode
 
 
@@ -28,6 +29,53 @@ class Declarations(QuadTranslatable):
         return QuadCode()
 # END   declarations #
 
+# START base statements #
+class Statement(QuadTranslatable):
+    pass
+
+class StatementList(Statement):
+    def __init__(self, statements: List[Statement] = []):
+        self.statements = statements
+
+    def add_statement(self, statement: Statement):
+        self.statements.append(statement)
+
+    def translate(self, quad_translator: QuadTranslator) -> QuadCode:
+        opcodes = ''
+        if not self.statements: return QuadCode()
+
+        for statement in self.statements:
+            last_quad_code = statement.translate(quad_translator)
+            opcodes += last_quad_code.opcodes + '\n'
+
+        return QuadCode(opcodes, last_quad_code.value_id)
+
+class StatementBlock(Statement):
+    def __init__(self, statements: StatementList):
+        self.statements = statements
+
+    def translate(self, quad_translator: QuadTranslator) -> QuadCode:
+        return self.statements.translate(quad_translator)
+# END   base statements #
+
+# START cases #
+@dataclass
+class DefaultCase:
+    statements: StatementList
+
+@dataclass
+class ConditionalCase:
+    number: int
+    statements: StatementList
+
+@dataclass
+class ConditionalCases:
+    cases: List[ConditionalCase]
+
+    def add_case(self, case: ConditionalCase):
+        self.cases.append(case)
+# END   cases #
+
 # START expressions #
 # TODO
 class Expression(QuadTranslatable):
@@ -39,9 +87,6 @@ class BooleanExpression(QuadTranslatable):
 # END   expressions #
 
 # START statements #
-class Statement(QuadTranslatable):
-    pass
-
 class InputStatement(Statement):
     def __init__(self, id: str):
         self.id = id
@@ -122,8 +167,10 @@ class IFStatement(Statement):
         opcodes = f'''
         {condition_evaluation_code.opcodes}
         JUMPZ {case_false_label_name} {condition_evaluation_code.value_id}
+
         {case_true_label_name}: {self.case_true.translate(quad_translator).opcodes}
-        JMP {post_case_false_label_name}
+        JUMP {post_case_false_label_name}
+
         {case_false_label_name}: {self.case_false.translate(quad_translator).opcodes}
         {post_case_false_label_name}:'''
 
@@ -141,38 +188,67 @@ class WhileStatement(Statement):
 
         opcodes = f'''
         {check_condition_label_name}: {condition_evaluation_code.opcodes}
-        JUMPZ {post_statement_label_name} {condition_evaluation_code.value_id}
+        JMPZ {post_statement_label_name} {condition_evaluation_code.value_id}
 
         {self.statement.translate(quad_translator).opcodes}
 
-        JMP {check_condition_label_name}
+        JUMP {check_condition_label_name}
         {post_statement_label_name}:'''
 
         return QuadCode(opcodes=opcodes)
 
-class StatementList(Statement):
-    def __init__(self, statements: List[Statement] = []):
-        self.statements = statements
-
-    def add_statement(self, statement: Statement):
-        self.statements.append(statement)
-
-    def translate(self, quad_translator: QuadTranslator) -> QuadCode:
-        opcodes = ''
-        if not self.statements: return QuadCode()
-
-        for statement in self.statements:
-            last_quad_code = statement.translate(quad_translator)
-            opcodes += last_quad_code.opcodes + '\n'
-
-        return QuadCode(opcodes, last_quad_code.value_id)
-
-class StatementBlock(Statement):
-    def __init__(self, statements: StatementList):
-        self.statements = statements
+class SwitchStatement(Statement):
+    def __init__(self, expression: Expression, conditional_cases: ConditionalCases, default_case: DefaultCase):
+        self.expression = expression
+        self.default_case = default_case
+        self.conditional_cases = conditional_cases
 
     def translate(self, quad_translator: QuadTranslator) -> QuadCode:
-        return self.statements.translate(quad_translator)
+        boolean_name = quad_translator.get_temp_boolean_name()
+        expression_code = self.expression.translate(quad_translator)
+        post_last_case_label_name = quad_translator.get_temp_label_name()
+
+        if not expression_code.value_id or quad_translator.get_variable_type(expression_code.value_id) == 'int':
+            raise SyntaxError('expression for switch statement must be integer')
+
+        label_to_conditional_case: Dict[str, ConditionalCase] = {
+            quad_translator.get_temp_label_name(): conditional_case
+            for conditional_case in self.conditional_cases
+        }
+
+        conditional_case_number_to_label: Dict[int, str] = {
+            conditional_case.number: label_name
+            for label_name, conditional_case in label_to_conditional_case.items()
+        }
+
+        if len(conditional_case_number_to_label) != len(label_to_conditional_case):
+            raise SyntaxError('Invalid duplicate cases')
+
+        # TODO: use RELop equals
+        compare_and_jump_case_opcodes = [
+            f'''
+            INQL {boolean_name} {expression_code.value_id} {case_number}
+            JMPZ {case_label} {boolean_name}
+            '''
+            for case_number, case_label in conditional_case_number_to_label.items()
+        ]
+
+        default_case_opcodes = f'''
+        {self.default_case.statements.translate(quad_translator).opcodes}
+        JUMP {post_last_case_label_name}
+        '''
+
+        opcodes = f'''
+        {compare_and_jump_case_opcodes}
+        {default_case_opcodes}
+        {'\n'.join(
+            f'{label_name}: {conditional_case.statements.translate(quad_translator).opcodes}'
+            for label_name, conditional_case
+            in label_to_conditional_case.items()
+        )}
+        {post_last_case_label_name}:'''
+
+        return QuadCode(opcodes=opcodes)
 # END   statements #
 
 # START AST #
